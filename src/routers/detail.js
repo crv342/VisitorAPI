37b0b6dc362eaa5233bb19a16b47a3f5b811787a
crv342 <chiragrvv@gmail.com>
2021-07-12 16:36:05 +0530

    purpose routes fixed

--- src/routers/detail.js
+++ src/routers/detail.js
@@ -4,7 +4,7 @@ const auth = require("../middleware/auth");
 const Detail = require("../model/detail");
 
 router.post("/host/add", auth, async (req, res) => {
-  let host;  
+  let host;
   var dataObj = await Detail.find();
 
   if (dataObj.length !== 0) {
@@ -22,7 +22,7 @@ router.post("/host/add", auth, async (req, res) => {
 
 router.delete("/host/remove/:id", auth, async (req, res) => {
   const data = await Detail.find();
-  
+
   if (data.length == 0) {
     res.status(404);
   }
@@ -40,6 +40,34 @@ router.get("/details",async (req, res) => {
     res.send(data[0])
 })
 
+router.post("/purpose/add", auth, async (req, res) => {
+    let purpose;
+    var dataObj = await Detail.find();
+
+    if (dataObj.length !== 0) {
+        dataObj[0].purpose.push(req.body);
+        await dataObj[0].save()
+        purpose = dataObj[0]
+    } else {
+        dataObj = new Detail({ purpose: req.body });
+        await dataObj.save();
+        purpose = dataObj;
+    }
+
+    res.send(purpose);
+});
+
+router.delete("/purpose/remove/:id", auth, async (req, res) => {
+    const data = await Detail.find();
+
+    if (data.length == 0) {
+        res.status(404);
+    }
+    const purpose = data[0].purpose.id(req.params.id).remove()
+    await data[0].save()
+    res.send(purpose);
+});
+
 router.put("/purpose", auth, async (req, res) => {
     const data = await Detail.find();
 
@@ -47,8 +75,8 @@ router.put("/purpose", auth, async (req, res) => {
         res.status(404);
       }
     data[0].purpose = req.body.purpose
-    await data[0].save() 
-    res.status(200).send(data[0].purpose)   
+    await data[0].save()
+    res.status(200).send(data[0].purpose)
 });
 
 module.exports = router;
